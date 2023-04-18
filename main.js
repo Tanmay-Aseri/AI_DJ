892a12a1a65400ae67b19d8e226b78a86aa1e4ef
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-04-18 16:23:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 song="";
+leftWristX = 0;
+leftWristY = 0;
+
+rigthWristX = 0;
+rightWristY = 0;
 
 function preload(){
     song = loadSound("music.mp3"); 
@@ -14,8 +19,32 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+    poseNet.on("pose", gotPoses);
 }
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
+
+ function modelLoaded(){
+    console.log("posenet is initialized");
+ }
+
+ function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX+ "leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX+ "rightWristY = "+rightWristY);
+    }
+ }
+
