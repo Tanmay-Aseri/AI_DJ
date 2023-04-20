9179873ab9fb13002f9825f7f08b59ebf33a55fc
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-04-20 16:28:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,29 @@ leftWristY = 0;
 rigthWristX = 0;
 rightWristY = 0;
 
+scoreLeftWrist = 0;
+
 function preload(){
     song = loadSound("music.mp3"); 
 }
 
 function draw(){
      image(video, 0, 0, 600, 500);
+
+     fill("#FF0000");
+     stroke("#FF0000");
+     circle(leftWristX, leftWristY, 20);
+
+     if(scoreLeftWrist > 0.2){
+
+     
+
+     InNumberleftWristY = Number(leftWristY);
+     remove_decimal = floor(InNumberleftWristY);
+     volume = remove_decimal/500;
+     document.getElementById("volume").innerHTML = "Volume = "+volume;
+     song.setVolume(volume);
+    }
 }
 
 function setup(){
@@ -38,6 +55,9 @@ function play(){
  function gotPoses(results){
     if (results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist" + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = "+leftWristX+ "leftWristY = "+leftWristY);
