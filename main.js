ff6c401f5639fe2568195dedbf9ee4138b60d838
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-04-25 16:24:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ song="";
 leftWristX = 0;
 leftWristY = 0;
 
-rigthWristX = 0;
+rightWristX = 0;
 rightWristY = 0;
 
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload(){
     song = loadSound("music.mp3"); 
@@ -16,11 +17,13 @@ function draw(){
 
      fill("#FF0000");
      stroke("#FF0000");
-     circle(leftWristX, leftWristY, 20);
+
+     circle(rightWristX, rightWristY, 20);
+     
 
      if(scoreLeftWrist > 0.2){
 
-     
+     circle(leftWristX, leftWristY, 20);
 
      InNumberleftWristY = Number(leftWristY);
      remove_decimal = floor(InNumberleftWristY);
@@ -28,6 +31,24 @@ function draw(){
      document.getElementById("volume").innerHTML = "Volume = "+volume;
      song.setVolume(volume);
     }
+if(scoreRightWrist > 0.2){
+    if( rightWristY > 0 && rightWristY <= 100  ){
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }else if(rightWristY > 100 && rightWristY <= 200){
+        document.getElementById("speed").innerHTML = "speed = 1x";
+        song.rate(1);
+    }else if(rightWristY > 200 && rightWristY <= 300){
+        document.getElementById("speed").innerHTML = "speed = 1.5x";
+        song.rate(1.5);
+    }else if(rightWristY > 300 && rightWristY <= 400){
+        document.getElementById("speed").innerHTML = "speed = 1x";
+        song.rate(2);
+    }else if(rightWristY > 400 && rightWristY <= 500){
+        document.getElementById("speed").innerHTML = "speed = 1x";
+        song.rate(2.5);
+    }
+}
 }
 
 function setup(){
@@ -56,7 +77,8 @@ function play(){
     if (results.length > 0){
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist" + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist" + scoreLeftWrist  +"scoreRightWrist" + scoreRightWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
